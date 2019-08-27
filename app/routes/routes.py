6bc00043d23d6b4f5e475163ae19f0a9d34c93d7
Richard Chen <richardchen39@gmail.com>
2019-08-27 16:09:06 -0700

    Add favicon and refactor HTML into templates

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,15 +1,7 @@
 from ..models import parser, script
-from flask import jsonify, render_template, request
+from flask import jsonify, render_template, request, send_from_directory
 from app import app
-
-@app.route('/', methods=['GET'])
-def index():
-  parser.parse_etherscan_data()
-  return render_template('covers.html')
-
-@app.route('/covers', methods=['GET'])
-def covers():
-  return jsonify(script.get_covers())
+import os
 
 @app.route('/active_cover_amount', methods=['GET'])
 def active_cover_amount():
@@ -19,12 +11,9 @@ def active_cover_amount():
 def active_cover_amount_per_contract():
   return jsonify(script.get_active_cover_amount_per_contract())
 
-####################################################################################################
-
-@app.route('/capital_pool', methods=['GET'])
-def capital_pool():
-  parser.parse_etherscan_data()
-  return render_template('capital_pool.html')
+@app.route('/covers', methods=['GET'])
+def covers():
+  return jsonify(script.get_covers())
 
 @app.route('/capital_pool_size', methods=['GET'])
 def capital_pool_size():
@@ -38,13 +27,6 @@ def capital_pool_distribution():
 def mcr_percentage():
   return jsonify(script.get_mcr_percentage())
 
-####################################################################################################
-
-@app.route('/staked', methods=['GET'])
-def staked():
-  parser.parse_etherscan_data()
-  return render_template('staked.html')
-
 @app.route('/total_amount_staked', methods=['GET'])
 def total_amount_staked():
   return jsonify(script.get_total_amount_staked())
@@ -53,13 +35,23 @@ def total_amount_staked():
 def amount_staked_per_contract():
   return jsonify(script.get_amount_staked_per_contract())
 
-####################################################################################################
-
-@app.route('/token', methods=['GET'])
-def token():
-  parser.parse_etherscan_data()
-  return render_template('token.html')
-
 @app.route('/nxm_token_price', methods=['GET'])
 def nxm_token_price():
   return jsonify(script.get_nxm_token_price())
+
+####################################################################################################
+
+@app.route('/<template>', methods=['GET'])
+def render(template):
+  parser.parse_etherscan_data()
+  return render_template(template + '.html')
+
+@app.route('/', methods=['GET'])
+def index():
+  parser.parse_etherscan_data()
+  return render_template('covers.html')
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.gif',
+      mimetype='image/vnd.microsoft.icon')
--- app/static/js/covers.js
+++ app/static/js/covers.js
@@ -1,19 +1,3 @@
-$.get('covers', (response) => {
-  let table = document.getElementById('covers')
-  for (cover of response) {
-    let row = ''
-    row += '<tr>'
-    row += '<td>' + cover['id'] + '</td>'
-    row += '<td>' + cover['contract_name'] + '</td>'
-    row += '<td>' + cover['amount'] + '</td>'
-    row += '<td>' + cover['start_time'] + '</td>'
-    row += '<td>' + cover['end_time'] + '</td>'
-    row += '</tr>'
-    table.innerHTML += row
-  }
-  $('#dataTable').DataTable();
-})
-
 $.get('active_cover_amount', (response) => {
   Plotly.newPlot('activeCoverAmount', [{
     x: Object.keys(response),
@@ -29,4 +13,20 @@ $.get('active_cover_amount_per_contract', (response) => {
     y: Object.values(response),
     type: 'bar'
   }])
+})
+
+$.get('covers', (response) => {
+  let table = document.getElementById('covers')
+  for (cover of response) {
+    let row = ''
+    row += '<tr>'
+    row += '<td>' + cover['id'] + '</td>'
+    row += '<td>' + cover['contract_name'] + '</td>'
+    row += '<td>' + cover['amount'] + '</td>'
+    row += '<td>' + cover['start_time'] + '</td>'
+    row += '<td>' + cover['end_time'] + '</td>'
+    row += '</tr>'
+    table.innerHTML += row
+  }
+  $('#dataTable').DataTable();
 })
