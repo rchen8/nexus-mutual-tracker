24188e6da10aff10d250041109e86d15bab1e148
Richard Chen <richardchen39@gmail.com>
2019-09-06 10:25:13 -0700

    Separate development and production servers

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import abort, Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
+import sys
 
 app = Flask(__name__, static_folder='static', static_url_path='')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/nexus'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku = Heroku(app)
+
+if len(sys.argv) == 1: # development server
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/nexus'
+elif len(sys.argv) == 2: # production server
+  heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 from . import routes as Routes
