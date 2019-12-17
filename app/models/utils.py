0a277f347d15f10634f446da3b9fd333d7fccada
Richard Chen <richardchen39@gmail.com>
2019-12-17 08:44:41 -0800

    Add Paraswap address

--- app/models/utils.py
+++ app/models/utils.py
@@ -121,6 +121,7 @@ def address_to_contract_name(address):
     '132030497cbc19d10cb2690ecf9690803519b5de': 'Paraswap',
     '6b158039b9678b7452f311deb12dd08c579dad26': 'Paraswap',
     '72338b82800400f5488eca2b5a37270ba3b7a111': 'Paraswap',
+    'f92c1ad75005e6436b4ee84e88cb23ed8a290988': 'Paraswap',
     '4aa42145aa6ebf72e164c9bbc74fbd3788045016': 'POA Network',
     'e1579debdd2df16ebdb9db8694391fa74eea201e': 'POA Network',
     '5b67871c3a857de81a1ca0f9f7945e5670d986dc': 'Set Protocol',
